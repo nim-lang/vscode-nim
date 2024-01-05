ece2c3df48495847e51f380cadc4648d61c4aa15
jmgomez <info@jmgomez.me>
2024-01-05 16:39:28 +0000

    checks for the nimlangserver version and shows an info message to the user if an update is needed

--- src/nimLsp.nim
+++ src/nimLsp.nim
@@ -1,4 +1,4 @@
-import std/jsconsole
+import std/[jsconsole, strutils, jsfetch, asyncjs, sugar, sequtils, options, strformat]
 import platform/[vscodeApi, languageClientApi]
 
 import platform/js/[jsNodeFs, jsNodePath, jsNodeCp, jsNodeUtil]
@@ -7,26 +7,99 @@ from std/strformat import fmt
 from tools/nimBinTools import getNimbleExecPath, getBinPath
 from spec import ExtensionState
 
-proc handleLspVersion(nimlangserver: cstring) =
+type LSPVersion = tuple[major: int, minor: int, patch: int]
+const MinimalLSPVersion = (1, 0, 0)
+
+proc `$` (v: LSPVersion):string = &"v{v.major}.{v.minor}.{v.patch}"
+
+proc `>`(v1, v2: LSPVersion): bool =
+  if v1.major != v2.major: v1.major > v2.major
+  elif v1.minor != v2.minor:  v1.minor > v2.minor
+  else: v1.patch > v2.patch
+
+proc `==`(v1, v2: LSPVersion): bool = 
+  v1.major == v2.major and v1.minor == v2.minor and 
+  v1.patch == v2.patch
+
+proc parseVersion(version: string): Option[LSPVersion] = 
+  #expected version = vMajor.Minor.Patch
+  var ver = version.split(".")
+  if ver.len != 3: return none(LSPVersion)
+  ver[0] = ver[0].replace("v", "")
+  var versions = newSeq[int]()
+  for v in ver:
+    try:
+      versions.add(parseInt(v.strip()))
+    except CatchableError: 
+      console.error("Error parsing version", v.cstring)
+      console.error(getCurrentExceptionMsg().cstring)
+      return none(LSPVersion)
+  some (versions[0], versions[1], versions[2])
+
+proc getLatestVersion(versions: seq[LSPVersion]): LSPVersion = 
+  result = versions[0]
+  for v in versions:
+    if v > result: result = v
+
+proc getLatestReleasedLspVersion(default: LSPVersion): Future[LSPVersion] {.async.} = 
+  type 
+    Tag = object of JsObject
+      name: cstring
+  proc toTags(obj: JsObject): seq[Tag] {.importjs: ("#").}
+  let url = " https://api.github.com/repos/nim-lang/langserver/tags".cstring
+  var failed = false
+  let res = await fetch(url)
+    .then((res: Response) => res.json())
+    .then((json: JsObject) => json)
+    .catch(proc(err: Error): JsObject = 
+      console.error("Nimlangserver request to GitHub failed", err)
+      failed = true
+    )
+  if failed: 
+    return default
+  else: 
+    return res
+    .toTags
+    .mapIt(parseVersion($it.name))
+    .filterIt(it.isSome)
+    .mapIt(it.get)
+    .getLatestVersion()
+
+proc notifyUserOnTheLSPVersion(current, latest: LSPVersion) = 
+  if latest > current:
+    let msg = &"""
+There is a new Nim langserver version available ({latest}). 
+You can install it by running: nimble install nimlangserver"""
+    vscode.window.showInformationMessage(msg.cstring)
+  else:
+    console.log("Your lsp version is updated")
+
+proc handleLspVersion(nimlangserver: cstring, latestVersion: LSPVersion) =
   var isDone = false
   proc onExec(error: ExecError, stdout: cstring, stderr: cstring) = 
-    let ver = ($stdout).split(".")
-    let (major, minor, patch) = (ver[0].cstring, ver[1].cstring, ver[2].cstring)
-    console.log("Major: ", major, " Minor: ", minor, " Patch: ", patch)
-    isDone = true
-    #TODO check the version with the latest released one
-  
+      let ver = parseVersion($stdout)
+      if ver.isNone():
+        console.error("Unexpected output from nimlangserver: ", stdout)
+      else:
+        isDone = true
+        notifyUserOnTheLSPVersion(ver.get, latestVersion) 
+
   var process: ChildProcess
   proc onLspTimeout() = 
-    if isDone: return #the process already quit
+    if isDone: return #the process already quit and the user is already notified
     #Running 0.2.0 kill the started nimlangserver process and notify the user is running an old version of the lsp
     kill(process)
-    #TODO check the version with the latest released one
+    notifyUserOnTheLSPVersion(MinimalLSPVersion, latestVersion)
 
-  global.setTimeout(onLspTimeout, 250)
+  global.setTimeout(onLspTimeout, 1000)
   process = cp.exec((nimlangserver & " --version"), ExecOptions(), onExec)    
 
-
+proc getLspPath(): cstring = 
+  var lspPath = vscode.workspace.getConfiguration("nim").getStr("lsp.path")
+  if lspPath.isNil or lspPath == "":
+    lspPath = getBinPath("nimlangserver")
+  console.log("Attempting to use nimlangserver at " & lspPath)
+  lspPath
 
 proc startLanguageServer(tryInstall: bool, state: ExtensionState) {.async.} =
   let rawPath = getLspPath()
@@ -49,7 +122,8 @@ proc startLanguageServer(tryInstall: bool, state: ExtensionState) {.async.} =
     let nimlangserver = path.resolve(rawPath);
     console.log(fmt"nimlangserver found: {nimlangserver}".cstring)
     console.log("Starting nimlangserver.")
-    handleLspVersion(nimlangserver)
+    let latestVersion = await getLatestReleasedLspVersion(MinimalLSPVersion)
+    handleLspVersion(nimlangserver, latestVersion)
 
     let
       serverOptions = ServerOptions{
