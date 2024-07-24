c9f57fbd21646e45e2f25692314e66217401de95
jmgomez <info@jmgomez.me>
2024-07-24 13:31:00 +0100

    Removes sample item

--- src/nimLsp.nim
+++ src/nimLsp.nim
@@ -471,7 +471,6 @@ proc getChildrenImpl(self: NimLangServerStatusProvider, element: LspItem = nil):
         newLspItem("Port", cstring($instance.port)),
         newLspItem("Open Files", instance.openFiles.join(", ").cstring),
         newLspItem("Unknown Files", instance.unknownFiles.join(", ").cstring),
-        newLspItem("Info:", "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.".cstring)    ]
 
     return @[]
 
