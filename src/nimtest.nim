bf0a2f3878683ac5b8ff03de724a89c9c05be3c7
Juan M Gómez <info@jmgomez.me>
2025-04-15 11:11:01 +0100

    Individual tests can be ran from the extension (#154)

--- src/nimtest.nim
+++ src/nimtest.nim
@@ -1,6 +1,9 @@
 import platform/vscodeApi
-import std/[strformat, jsconsole, tables]
+import std/[strformat, jsconsole, tables, options, sequtils]
 import spec, nimLsp
+import platform/js/jsNodeFs
+import nimProjects
+import nimUtils
 
 var testController: VscodeTestController
 
@@ -16,32 +19,66 @@ run.end() - Complete the test run
 
 #Basically in nim you will be able to run all tests a suite, or a single test
 
+proc renderTestResult(test: VscodeTestItem, result: RunTestResult, run: VscodeTestRun) =
+  console.log("Rendering test result: ", result)
+  let duration = result.time * 1000
+  console.log("Duration: ", duration)
+  if result.failure.isNull:
+    run.passed(test, duration = duration)
+    run.appendOutput(&"[{test.label}] Test passed in {duration:.4f}ms\n")
+  else:
+    run.failed(test, VscodeTestMessage(message: result.failure), duration = duration)  # Use time directly
+    run.appendOutput(&"[{test.label}] Test failed with error:\n")    
+    run.appendOutput(result.failure)
+    run.appendOutput(&"[{test.label}] Test failed in {duration:.4f}ms\n")
 
-proc runSingleTest(test: VscodeTestItem) =
-  let run = testController.createTestRun(test)
+proc runSingleTest(test: VscodeTestItem, run: VscodeTestRun) = 
+  let state = ext
+  let entryPoint = state.config.getStrArray("test.entryPoints")[0]
+      
   run.started(test)
   console.log("Running test: ", test.id)
+  let runTestParams = RunTestParams(testNames: @[test.id], entryPoints: @[entryPoint])
+  let runTestRes = requestRunTest(state, runTestParams)
+  runTestRes.then(proc(res: RunTestProjectResult) =
+    console.log("Run test result: ", res)
+    renderTestResult(test, res.suites[0].testResults[0], run)
+    run.`end`()
+  )
+  # runTestRes.catch(proc(err: ref Exception) =
+  #   console.log("Run test error: ", err)
+  #   run.failed(test, VscodeTestMessage(message: err.msg), duration = 0)
+  #   run.`end`()
+  # )
   
-  if test.children.size() == 0:
-    run.appendOutput(&"\n[{test.label}] Starting test...\n")
+  
+
+
+# proc runSingleTest(test: VscodeTestItem) =
+#   let run = testController.createTestRun(test)
+#   run.started(test)
+#   console.log("Running test: ", test.id)
+  
+#   if test.children.size() == 0:
+#     run.appendOutput(&"\n[{test.label}] Starting test...\n")
     
-    global.setTimeout(proc() =
-      if test.id == "test2":
-        run.appendOutput(&"[{test.label}] Test failed with error:\n")
-        run.appendOutput("Expected 42 but got 41\n")
-        run.failed(test, "Failed test".cstring)
-      else:
-        run.appendOutput(&"[{test.label}] Test completed successfully\n")
-        run.appendOutput("All assertions passed\n")
-        run.passed(test, "Passed test".cstring)
+#     global.setTimeout(proc() =
+#       if test.id == "test2":
+#         run.appendOutput(&"[{test.label}] Test failed with error:\n")
+#         run.appendOutput("Expected 42 but got 41\n")
+#         run.failed(test, "Failed test".cstring)
+#       else:
+#         run.appendOutput(&"[{test.label}] Test completed successfully\n")
+#         run.appendOutput("All assertions passed\n")
+#         run.passed(test, "Passed test".cstring)
     
-    , 1000)
-  else:
-    console.log("Running suite: ", test.id)
-    run.appendOutput(&"\n=== Running Suite: {test.label} ===\n")
-    test.children.forEach(proc(child: VscodeTestItem) =
-      runSingleTest(child)
-    )
+#     , 1000)
+#   else:
+#     console.log("Running suite: ", test.id)
+#     run.appendOutput(&"\n=== Running Suite: {test.label} ===\n")
+#     test.children.forEach(proc(child: VscodeTestItem) =
+#       runSingleTest(child)
+#     )
 
 proc runHandler(request: VscodeTestRunRequest, token: VscodeCancellationToken) =
   console.log("Running tests...", request)
@@ -50,18 +87,15 @@ proc runHandler(request: VscodeTestRunRequest, token: VscodeCancellationToken) =
   if isRunAll:
       let items = testController.getItems()
       items.forEach(proc(item: VscodeTestItem) =
-        runSingleTest(item)
+        let run: VscodeTestRun = testController.createTestRun(request)
+        runSingleTest(item, run)
       )
   else:
-    let run = testController.createTestRun(request)
+    let run: VscodeTestRun = testController.createTestRun(request)
     for item in request.include:
-      runSingleTest(item)
+      runSingleTest(item,run)
 
     console.log("Include array: ", request.include)
-
-    global.setTimeout(proc() =
-      run.`end`()
-    , 3000)
 
 
 proc initializeTests*(context: VscodeExtensionContext, state: ExtensionState) =
@@ -69,21 +103,36 @@ proc initializeTests*(context: VscodeExtensionContext, state: ExtensionState) =
     proc inner() {.async.} = 
       if excRunTests notin state.lspExtensionCapabilities:
         console.log("Run tests capability not found")
-        return
+        return      
       #Only one for now
       var entryPoint = state.config.getStrArray("test.entryPoints")[0]
+      
+      
       console.log("Entry point: ", entryPoint)
       let listTestsParams = ListTestsParams(entryPoints: @[entryPoint])
       let listTestsRes = await fetchListTests(state, listTestsParams)
       testController = vscode.tests.createTestController("nim-tests".cstring, "Nim Tests".cstring)
+      #show message is there are not tests:
+      if listTestsRes.projectInfo.suites.keys.toSeq.len == 0:
+        vscode.window.showInformationMessage("No tests found for entry point: " & entryPoint)
+        return
 
-      console.log("List tests result: ", listTestsRes)
+
       for key, suite in listTestsRes.projectInfo.suites:
         let suiteItem = testController.createTestItem(suite.name, suite.name)
         for test in suite.tests:
           let testItem = testController.createTestItem(test.name, test.name)
           suiteItem.children.add(testItem)
         testController.getItems().add(suiteItem)
+
+      discard testController.createRunProfile(
+        "Run Tests",
+        VscodeTestRunProfileKind.Run,
+        runHandler,
+        true
+      )
+      
+
 
       # console.log("Creating test controller...")
       
